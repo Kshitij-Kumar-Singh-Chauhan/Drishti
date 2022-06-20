32585c01838911094cf4576cf4ce46d59913490c
Kshitij-Kumar-Singh-Chauhan <76607445+Kshitij-Kumar-Singh-Chauhan@users.noreply.github.com>
2022-06-20 15:31:37 +0530

    Delete js directory

--- js/snack.js
+++
@@ -1,5 +0,0 @@
-function snack() {
-    var x = document.getElementById("snackbar");
-    x.className = "show";
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 2000);
-}
